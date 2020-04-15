cccd3da20ab3f9d36570b98d48fa151a8b8e729b
Pavel Svinticki <57325225+DevilOfWar@users.noreply.github.com>
2020-04-15 11:21:02 +0300

    Fix some bad coding.
    Fix some bad coding.

---
+++ Lab1/Lab1/LogsAndExceptions/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Lab1
+{
+    public static class Logger
+    {
+        public static string lastMessage { get; private set; }
+        public static void Log(string message)
+        {
+            using (StreamWriter file = new StreamWriter("D:\\logs.log", true))
+            {
+                file.WriteLine(message);
+                lastMessage = message;
+            }
+        }
+    }
+}
